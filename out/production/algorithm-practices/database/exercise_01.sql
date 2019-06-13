f42e5133964f1d17b1c2d1c4197860d43768b890
ShannonSun <504582433@qq.com>
2019-06-13 23:35:39 +0800

    .

--- out/production/algorithm-practices/database/exercise_01.sql
+++
@@ -1,9 +0,0 @@
-SELECT Score, (SELECT count(DISTINCT score)
-               FROM Scores
-               WHERE score >= s.score) AS Rank
-FROM Scores s
-ORDER BY Score DESC ;
-
-select a.Score, sum(case when b.Score>=a.Score then 1 end) as Rank
-from Scores a, (select distinct Score from Scores ) b
-group by a.id order by a.Score desc;
