53e140c2c8aeba45b523019bca4e18dd85697b3d
sunxibin <sunxibin@flashhold.com>
2019-07-02 14:49:56 +0800

    .

---
+++ src/algorithm/mathematics/math/SimpleTest.java
@@ -0,0 +1,16 @@
+package algorithm.mathematics.math;
+
+/**
+ * @author sunxibin
+ */
+public class SimpleTest {
+
+    public static void main(String[] args) {
+        function_001();
+    }
+
+    public static void function_001() {
+        int i = -1;
+        System.out.println(i/2);
+    }
+}
