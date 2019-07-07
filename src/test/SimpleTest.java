c8f48b3b1c7d9cd37f6d92ac28cc4b2f50f06f88
ShannonSun <504582433@qq.com>
2019-07-07 12:31:43 +0800

    。

--- src/test/SimpleTest.java
+++ src/test/SimpleTest.java
@@ -6,16 +6,36 @@ package test;
  */
 public class SimpleTest {
     public static void main(String[] args) {
-        function01();
+        function001();
     }
 
     /**
      * 测试for循环
      */
-    public static void function01() {
+    public static void function001() {
         int i = 0 ;
         for (; i < 1; i++) {
             System.out.println("i = " + i);
         }
     }
+
+    /**
+     * 取余
+     */
+    public static void function_002() {
+        int i = -1;
+        System.out.println(i/2);
+    }
+
+    /**
+     * 位运算
+     */
+    public static void function_003() {
+        int a = -3;
+        int b = a >> 1;
+        int c = a >>> 1;
+        System.out.println("b = " + b + "\n" + "c = " + c);
+        System.out.println("2^32 - 1 = " + (1 << 32 - 1));
+    }
+
 }
